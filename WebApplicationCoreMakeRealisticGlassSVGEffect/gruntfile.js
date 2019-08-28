5ca8b91b4e263c71ad5d0b6b4673643147faf08d
Steven Van Iseghem <steven.van.iseghem@outlook.com>
2019-08-28 07:21:34 +0200

    Add project files.

---
+++ WebApplicationCoreMakeRealisticGlassSVGEffect/gruntfile.js
@@ -0,0 +1,31 @@
+﻿/// <binding BeforeBuild='default' />
+
+module.exports = function (grunt) {
+
+    const sass = require('node-sass');
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: [{
+                    expand: true, // Recursive
+
+                    cwd: "sass", // The startup directory
+
+                    src: ["**/*.scss"], // Source files
+
+                    dest: "wwwroot", // Destination
+
+                    ext: ".css" // File extension
+                }]
+            }
+        }
+    });
+
+    // Load the plugin
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.registerTask('default', ['sass']);
+};
---
+++ WebApplicationCoreMakeRealisticGlassSVGEffect/wwwroot/site.js
@@ -0,0 +1,23 @@
+﻿var divxPos = 0;
+
+
+
+window.onload = function () {
+
+    runCode();
+
+};
+
+
+
+function runCode() {
+
+    var test = document.getElementById("testElement");
+
+    test.style.left = divxPos++ + 'px';
+
+
+
+    setTimeout(() => runCode(), 50);
+
+};
